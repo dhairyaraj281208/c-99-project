ef9a8ca69c533f1259d7e8eed9e532a22f917829
dhairyaraj281208 <72195034+dhairyaraj281208@users.noreply.github.com>
2021-05-17 15:51:27 +0530

    Add files via upload

---
+++ Removefiles.py
@@ -0,0 +1,26 @@
+import time
+import shutil
+import os
+
+# Making a variable of path
+path = 'C:\\Users\\Dhairya\\Desktop\\bla.txt'
+
+# Creating a function for getting the time for the path and converting it into days
+def time():
+    cTime = os.stat(path).st_ctime
+    seconds = cTime//1000
+    minutes = seconds//60
+    hours = minutes//60
+    days = hours//24
+    time = days
+
+    return time
+
+def remove():
+    time1 = time()
+    if(time1 <= 31.0):
+        os.remove(path)
+    else:
+        b = 0
+
+remove()
